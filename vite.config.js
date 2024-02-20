8af5d45e51ab28f8e89415fe9d3abfcd601dcdbe
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-20 02:55:37 +0200

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { resolve } from 'node:path';
+// import copy from './node_modules/vite-plugin-copy/dist/vite-plugin-copy.js';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
