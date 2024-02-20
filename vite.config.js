ae1a03366a9fa0f9367e783bbf0b8b8cd936a47c
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-20 02:14:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig((command) => ({
     base: command === 'build' ? '/UkrainianHouse/' : '/',
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src/'),
+        },
+    },
 }));
