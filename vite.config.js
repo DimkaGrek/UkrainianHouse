3d44c087c1ae180258ca8cfdba97f0dbac804058
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-20 02:26:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig((command) => ({
-    base: command === 'build' ? '/UkrainianHouse/' : '/',
+export default defineConfig(({ mode }) => ({
+    base: mode === 'production' ? '/UkrainianHouse/' : '/',
     plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
