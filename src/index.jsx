4d22800492351593ab660d0292d0761d09068624
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-20 02:29:37 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>
+    </BrowserRouter>
 );
