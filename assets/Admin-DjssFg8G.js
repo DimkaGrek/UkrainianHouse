9aca0eb3dad9a0428c400c3e4aacfbb345213ab1
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-20 00:15:13 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@ae1a03366a9fa0f9367e783bbf0b8b8cd936a47c 🚀

--- assets/Admin-DjssFg8G.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-C1lQ4raF.js";const i=()=>r.jsx("div",{children:"Admin"});export{i as default};
--- assets/LoginPage-C15Vi6Jr.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C1lQ4raF.js";const r=()=>e.jsx("div",{children:"LoginPage"});export{r as default};
