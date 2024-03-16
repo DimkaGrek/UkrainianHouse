94d9745601e438919e67ab16a3b740ea14bbeaa3
Sergey Kabanets <skabanets97@gmail.com>
2024-03-16 20:45:45 +0200

    change icon component and set close icon for modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Icon } from '../Icon/Icon';
 
 export const Modal = ({ children, toggleModal }) => {
   useEffect(() => {
@@ -28,13 +29,13 @@ export const Modal = ({ children, toggleModal }) => {
       className="flex items-center justify-center fixed bg-[#FFD437] bg-opacity-40 w-screen h-screen left-0 top-0 z-100"
       onClick={handleClickOnBackdrop}
     >
-      <div className="relative bg-white rounded-[30px] py-[35px] px-[80px] h-auto">
+      <div className="relative bg-white rounded-[30px] py-[50px] px-[50px] h-auto">
         <button
           type="button"
           onClick={toggleModal}
-          className="absolute top-[25px] right-[25px] w-[32px] h-[32px] bg-red-500"
+          className="absolute top-[20px] right-[20px] w-[20px] h-[20px]"
         >
-          X
+          <Icon name="close" className="fill-[#1E1E1E]" size="20" />
         </button>
         {children}
       </div>
