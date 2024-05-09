f9b437aebab60c50122fc158c4f9ba8869cf47cf
VasylPopaz <pafos1997@gmail.com>
2024-05-09 18:16:25 +0200

    fixed routes

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/UkrainianHouse">
     <App />
   </BrowserRouter>
 );
