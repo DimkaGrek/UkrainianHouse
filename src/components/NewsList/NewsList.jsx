961ec42d337354d3e6afee466777092bb1ba0fca
VasylPopaz <pafos1997@gmail.com>
2024-05-10 23:15:34 +0200

    added LoadMoreButton and hover, focus for NewsList

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -29,7 +29,7 @@ export const NewsList = () => {
   ];
 
   return (
-    <ul className="flex flex-col gap-[16px] md:flex-row md:flex-wrap md:gap-x-[20px] gap-y-[16px] lg:gap-x-[24px] lg:gap-y-[44px]">
+    <ul className="flex flex-col row-gap-[16px] mb-[34px] md:mb-[40px] lg:mb-[44px] md:flex-row md:flex-wrap md:gap-x-[20px] gap-y-[16px] lg:gap-x-[24px] lg:gap-y-[44px]">
       {news.map((item, index) => {
         return <NewsListItem key={index} {...item} />;
       })}
