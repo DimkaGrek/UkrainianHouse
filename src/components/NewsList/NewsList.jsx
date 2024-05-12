d580d9ec8aae15bb3d4b732a09dd5df4fb8420e8
VasylPopaz <pafos1997@gmail.com>
2024-05-12 13:44:38 +0200

    fixed news description

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -4,7 +4,8 @@ export const NewsList = () => {
   const news = [
     {
       title: `A Magical Day at the Children's Carnival`,
-      description: `Grateful for the memories created! An enchanting event unfolds at the Carnival, where the air is filled with the lively melodies of traditional Carnival songs and the joyous laughter of children...`,
+      description: `Grateful for the memories created! An enchanting event unfolds at the Carnival, where the air is filled with the lively melodies of traditional Carnival songs and the joyous laughter of children
+     `,
       date: '01-02-2024',
       author: 'Maastricht',
     },
