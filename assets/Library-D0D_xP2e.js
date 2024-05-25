f5e06bd80c6dff42006ceaf391d1f328e41f195f
skabanets <79872707+skabanets@users.noreply.github.com>
2024-05-25 17:48:34 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@1bd35961ab54e3a93d1b6a7ab9bac4721195c47d 🚀

---
+++ assets/Library-D0D_xP2e.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Djcdtb1p.js";const r=()=>e.jsxs("div",{children:["Library",e.jsx("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam labore inventore earum, expedita excepturi nam perferendis temporibus alias modi consequatur optio reiciendis saepe a nemo exercitationem error, soluta quibusdam assumenda?"})]});export{r as default};
