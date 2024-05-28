69adc70f9e4e7f8b9a5d5344ecec8b6cf9e6652f
VasylPopaz <pafos1997@gmail.com>
2024-05-28 18:22:06 +0200

    updated styles NewsListItem

--- src/components/Advertisement/Advertisement.jsx
+++ src/components/Advertisement/Advertisement.jsx
@@ -13,7 +13,7 @@ export const Advertisement = () => {
   } = getImages();
 
   return (
-    <div className="flex flex-col-reverse items-center gap-[16px]  bg-[#fff5c6] p-[18px] rounded-[18px] mb-[34px] md:flex-row md:gap-[12px] md:p-[24px] md:bg-[#ffd437] md:bg-opacity-40 lg:gap-[40px] lg:bg-[#fff6d5]  lg:bg-opacity-50 lg:py-[57px] lg:px-[40px] lg:mb-[44px] ">
+    <div className="flex flex-col-reverse items-center gap-[16px]  bg-[#fff5c6] p-[18px] rounded-[18px] mb-[34px] md:flex-row md:gap-[12px] md:p-[24px] md:bg-[#ffd437] md:bg-opacity-40 lg:gap-[40px] lg:bg-opacity-50 lg:py-[57px] lg:px-[40px] lg:mb-[44px] shadow-sm">
       <div className="lg:w-[748px]">
         <h3 className="font-proza-semibold text-[20px] font-semibold text-[#1e1e1e] mb-[4px] leading-[160%] md:font-proza-medium md:font-medium md:text-[26px] md:leading-[131%] md:mb-[8px] lg:font-proza-semibold lg:font-semibold lg:text-[36px] lg:leading-[111%] lg:mb-[20px] ">
           Coming soon...
