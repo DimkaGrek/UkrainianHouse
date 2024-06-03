fa3bb9dd7c6a2c9f7e6e9bb8c9bdc369c41cfdde
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-06-03 20:15:30 +0300

    add site developers adaptive for tablet

--- src/helpers/getDevelopers.js
+++ src/helpers/getDevelopers.js
@@ -61,7 +61,7 @@ export const getDevelopers = () => {
     },
     {
       name: 'Tatiana Mihush',
-      url: '',
+      url: 'https://www.linkedin.com/in/%D1%82%D0%B0%D1%82%D1%8C%D1%8F%D0%BD%D0%B0-%D0%BC%D0%B8%D0%B3%D1%83%D1%88-064216269/',
       position: 'Illustrator',
       photos: [mihush_desktop_1x, mihush_desktop_2x],
     },
