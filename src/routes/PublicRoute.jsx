b0d0240946419831d926de577b4d158840d1c9cc
Sergey Kabanets <skabanets97@gmail.com>
2024-10-18 22:04:19 +0300

    Add redux for admin auth

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 
+import { selectIsLoggedIn } from '../my-redux/Auth/authSlice';
+
 export const PublicRoute = ({ children }) => {
-  //   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = false;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   const location = useLocation();
 
   if (isLoggedIn && location.pathname.includes('/auth/')) {
