0ce07a3cbb388fb37ce893581999a91f39bb6ca1
Sergey Kabanets <skabanets97@gmail.com>
2024-10-18 22:05:08 +0300

    Update api config

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://ukrhouse.pp.ua/api',
 });
+
+export const setToken = token => {
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const clearToken = () => {
+  api.defaults.headers.common.Authorization = ``;
+};
