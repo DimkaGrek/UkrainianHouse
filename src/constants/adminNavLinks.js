552de7a46dca0ae9e9621d9f317468a28bed85d5
Sergey Kabanets <skabanets97@gmail.com>
2024-10-19 21:55:14 +0300

    Add sidebar for admin page

---
+++ src/constants/adminNavLinks.js
@@ -0,0 +1,5 @@
+export const adminNavLinks = [
+  { id: 1, text: 'News', link: '/admin/news' },
+  { id: 2, text: 'Books', link: '/admin/books' },
+  { id: 3, text: 'Inbox', link: '/admin/inbox' },
+];
