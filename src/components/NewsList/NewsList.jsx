a66ed9984b053561b72253449ccac75f8d37e33b
VasylPopaz <pafos1997@gmail.com>
2024-10-22 22:09:46 +0200

    upd hook useNews

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -3,17 +3,13 @@ import { NewsListItem } from './NewsListItem';
 import { useNews } from '../../hooks';
 
 export const NewsList = () => {
-  const { filteredNews, filter } = useNews();
+  const { news } = useNews();
 
-  return filteredNews.length ? (
+  return (
     <ul className="flex flex-col row-gap-[16px] mb-[34px] md:mb-[40px] lg:mb-[44px] md:flex-row md:flex-wrap md:gap-x-[20px] gap-y-[16px] lg:gap-x-[24px] lg:gap-y-[44px]">
-      {filteredNews.map((item, index) => {
+      {news.map((item, index) => {
         return <NewsListItem key={index} item={item} />;
       })}
     </ul>
-  ) : (
-    <h2 className="text-[#1a1a1a] text-3xl font-bold text-center mb-4">
-      No results for &quot;{filter}&quot;.
-    </h2>
   );
 };
