b040d083c33a25f2b5bf4ea3d20d9e38ae292a13
VasylPopaz <pafos1997@gmail.com>
2024-10-22 22:09:46 +0200

    upd hook useNews

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -3,30 +3,25 @@ import { useSelector } from 'react-redux';
 import {
   selectError,
   selectIsLoadingNews,
-  selectNews,
   selectAnnounceNews,
-  selectTotalNews,
-  selectFilter,
-  selectFilteredNews,
+  selectTotalPagesNews,
   selectPageNews,
+  selectNews,
 } from '../my-redux';
 
 export const useNews = () => {
   const news = useSelector(selectNews);
-  const filteredNews = useSelector(selectFilteredNews);
   const announceNews = useSelector(selectAnnounceNews);
   const page = useSelector(selectPageNews);
-  const totalNews = useSelector(selectTotalNews);
-  const filter = useSelector(selectFilter);
+  const totalPages = useSelector(selectTotalPagesNews);
   const isLoading = useSelector(selectIsLoadingNews);
   const error = useSelector(selectError);
+
   return {
     news,
-    filteredNews,
     announceNews,
     page,
-    totalNews,
-    filter,
+    totalPages,
     isLoading,
     error,
   };
