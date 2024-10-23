fe2d009ac1f820dfcb19bae27b10914282fe9c2f
VasylPopaz <pafos1997@gmail.com>
2024-10-23 19:54:41 +0200

    upd newsSlice

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -7,21 +7,27 @@ import {
   selectTotalPagesNews,
   selectPageNews,
   selectNews,
+  selectIsMoreNews,
+  selectHomeNews,
 } from '../my-redux';
 
 export const useNews = () => {
   const news = useSelector(selectNews);
+  const homeNews = useSelector(selectHomeNews);
   const announceNews = useSelector(selectAnnounceNews);
   const page = useSelector(selectPageNews);
   const totalPages = useSelector(selectTotalPagesNews);
+  const isMoreNews = useSelector(selectIsMoreNews);
   const isLoading = useSelector(selectIsLoadingNews);
   const error = useSelector(selectError);
 
   return {
     news,
+    homeNews,
     announceNews,
     page,
     totalPages,
+    isMoreNews,
     isLoading,
     error,
   };
