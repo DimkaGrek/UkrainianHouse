49cbebc110cb58f775c2b2fb56ff4890e9d824f0
VasylPopaz <pafos1997@gmail.com>
2024-10-26 20:10:13 +0200

    add baseUrl to image link in news item

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -104,7 +104,7 @@ const NewsPage = () => {
   return (
     <>
       <section className="pb-[74px] md:pb-[50px] lg:pb-[110px]">
-        <div className="hidden  md:flex justify-between items-center mb-[40px] lg:mb:[44px]">
+        <div className="hidden  md:flex justify-between items-center mb-[40px] lg:mb-[44px]">
           <h3 className="font-proza-semibold font-semibold text-[20px] text-[#222] leading-[160%] lg:font-proza-medium lg:font-medium lg:text-[60px] lg:leading-[130%]">
             News
           </h3>
