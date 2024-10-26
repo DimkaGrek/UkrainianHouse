945a3de9b25fa6e77bfee6f1bae91942d782504c
VasylPopaz <pafos1997@gmail.com>
2024-10-26 20:04:20 +0200

    upd newsSlice

--- src/my-redux/News/newsSlice.js
+++ src/my-redux/News/newsSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 
 import {
+  archiveOneNews,
   createNews,
   deleteOneNews,
   fetchAllNews,
@@ -15,6 +16,7 @@ const initialState = {
   oneNews: null,
   page: 0,
   totalPages: 0,
+  totalNews: 0,
   isMoreNews: true,
   isLoading: false,
   error: null,
@@ -24,7 +26,7 @@ const newsSlice = createSlice({
   name: 'news',
   initialState,
   reducers: {
-    setPage: (state, { payload }) => {
+    setPageNews: (state, { payload }) => {
       state.page = payload;
     },
     clearNews: state => {
@@ -36,14 +38,18 @@ const newsSlice = createSlice({
     builder
       .addCase(
         fetchAllNews.fulfilled,
-        (state, { payload: { currentPage, totalPages, news } }) => {
-          if (currentPage === 0) {
+        (
+          state,
+          { payload: { currentPage, totalPages, totalNews, news }, meta }
+        ) => {
+          if (meta.arg.isAdmin) {
             state.news = news;
           } else {
             state.news.push(...news);
           }
 
           state.totalPages = totalPages;
+          state.totalNews = totalNews;
           state.isMoreNews = currentPage + 1 < totalPages;
 
           state.isLoading = false;
@@ -59,10 +65,20 @@ const newsSlice = createSlice({
         state.isLoading = false;
       })
       .addCase(createNews.fulfilled, (state, { payload }) => {
-        state.news = [...state.news, payload];
+        state.news.unshift(payload);
         state.isLoading = false;
       })
       .addCase(updateNews.fulfilled, (state, { payload }) => {
+        state.news = state.news.map(item => {
+          if (item.id === payload.id) {
+            return payload;
+          }
+          return item;
+        });
+        state.isLoading = false;
+      })
+      .addCase(archiveOneNews.fulfilled, (state, { payload }) => {
+        // state.news = state.news.filter(item => item.id !== payload.id);
         state.news = state.news.map(item => {
           if (item.id === payload.id) {
             return payload;
@@ -110,17 +126,19 @@ const newsSlice = createSlice({
     selectAnnounceNews: state => state.announceNews,
     selectPageNews: state => state.page,
     selectTotalPagesNews: state => state.totalPages,
+    selectTotalNews: state => state.totalNews,
     selectIsMoreNews: state => state.isMoreNews,
     selectIsLoadingNews: state => state.isLoading,
     selectError: state => state.error,
   },
 });
-export const { setPage, clearNews } = newsSlice.actions;
+export const { setPageNews, clearNews } = newsSlice.actions;
 export const {
   selectNews,
   selectAnnounceNews,
   selectPageNews,
   selectTotalPagesNews,
+  selectTotalNews,
   selectIsMoreNews,
   selectIsLoadingNews,
   selectError,
