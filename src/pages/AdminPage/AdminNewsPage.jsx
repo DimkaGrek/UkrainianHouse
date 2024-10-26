103cdd0b4cde90bda501e32f08e29aa6f060e773
VasylPopaz <pafos1997@gmail.com>
2024-10-26 20:05:56 +0200

    upd AdminNewsPage

--- src/pages/AdminPage/AdminNewsPage.jsx
+++ src/pages/AdminPage/AdminNewsPage.jsx
@@ -1,5 +1,36 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { ContentList, Pagination } from '../../components';
+
+import { clearNews, fetchAllNews, setPageNews } from '../../my-redux';
+import { useNews } from '../../hooks';
+
 const AdminNewsPage = () => {
-  return <div>AdminNewsPage</div>;
+  const { news, page, totalNews, totalPages } = useNews();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(clearNews());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const config = { params: { page, size: 7 }, isAdmin: true };
+
+    dispatch(fetchAllNews(config));
+  }, [dispatch, page]);
+
+  const handleSetPage = page => {
+    dispatch(setPageNews(page));
+  };
+
+  return (
+    <section className="py-5">
+      <h2 className="text-[24px] font-medium mb-6">Total news: {totalNews}</h2>
+      <ContentList items={news} />
+      <Pagination setPage={handleSetPage} page={page} totalPages={totalPages} />
+    </section>
+  );
 };
 
 export default AdminNewsPage;
