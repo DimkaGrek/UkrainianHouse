f2ad4b57d981c8007403dea7492b463f5d6fd22a
Sergey Kabanets <skabanets97@gmail.com>
2024-10-27 20:53:18 +0200

    Add markup and add styles for book list items

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -2,7 +2,7 @@ import { ContentListItem } from './ContentListItem';
 
 export const ContentList = ({ items }) => {
   return (
-    <ul className="pr-2 flex flex-col gap-8 h-[70vh] max-h-[70vh] scrollbar mb-6">
+    <ul className="pr-2 flex flex-col h-[70vh] max-h-[70vh] scrollbar mb-6">
       {items.map(item => (
         <ContentListItem key={item.id} item={item} />
       ))}
