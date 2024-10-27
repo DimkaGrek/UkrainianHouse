34ce2705a576d896f7fd3879bda48a848079e4d0
VasylPopaz <145382665+VasylPopaz@users.noreply.github.com>
2024-10-27 23:49:24 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@5df9deec11156556b7551e77860244d8f733bbee 🚀

--- assets/AdminInboxPage-DC5SmhIY.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CZDjmvx5.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
---
+++ assets/AdminInboxPage-DR5JiEF8.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-DK6ajgEU.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
