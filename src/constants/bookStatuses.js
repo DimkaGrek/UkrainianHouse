375c2ffa738e4432ce21e8ec8b9444c7f3fc0b48
Sergey Kabanets <skabanets97@gmail.com>
2024-10-27 20:51:00 +0200

    Add functionfor book status colors

--- src/constants/bookStatuses.js
+++ src/constants/bookStatuses.js
@@ -10,3 +10,16 @@ export const bookStatuses = [
   'REFERENCEONLY',
   'ELECTRONICCOPY',
 ];
+
+export const bookStatusesColors = {
+  AVAILABLE: '#4ec253',
+  CHECKEDOUT: '#f4a261',
+  RESERVED: '#ffca3a',
+  INPROCESSING: '#5bc0de',
+  UNDERRAPAIR: '#6c757d',
+  OVERDUE: '#ff6b6b',
+  LOST: '#d9534f',
+  WITHDRAWN: '#9e9e9e',
+  REFERENCEONLY: '#8e44ad',
+  ELECTRONICCOPY: '#007bff',
+};
