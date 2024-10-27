69c459ba86ecc97d9c805d78660a79359d7bfd8e
Sergey Kabanets <skabanets97@gmail.com>
2024-10-27 19:30:57 +0200

    Add markup for AdminBooksPage

--- src/pages/AdminPage/AdminBooksPage.jsx
+++ src/pages/AdminPage/AdminBooksPage.jsx
@@ -1,5 +1,38 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { ContentList, Pagination } from '../../components';
+
+import { useBooks } from '../../hooks';
+import { clearBooks, fetchAllBooks, setPageBooks } from '../../my-redux';
+
 const AdminBooksPage = () => {
-  return <div>AdminBooksPage</div>;
+  const { books, page, totalBooks, totalPages } = useBooks();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(clearBooks());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const config = { params: { page, size: 7 }, isAdmin: true };
+
+    dispatch(fetchAllBooks(config));
+  }, [dispatch, page]);
+
+  const handleSetPage = page => {
+    dispatch(setPageBooks(page));
+  };
+
+  return (
+    <section className="py-5">
+      <h2 className="text-[24px] font-medium mb-6">
+        Total books: {totalBooks}
+      </h2>
+      <ContentList items={books} />
+      <Pagination setPage={handleSetPage} page={page} totalPages={totalPages} />
+    </section>
+  );
 };
 
 export default AdminBooksPage;
