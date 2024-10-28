ce54a1c8533b398747f33467fd5df7a800753003
NiukaloTetiana <146371274+NiukaloTetiana@users.noreply.github.com>
2024-10-28 20:22:08 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@c8dbfe885679e2499856b1fe6eda85e9187d11cb 🚀

--- assets/AdminInboxPage-BYlg2BC2.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-DHs-3ir3.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
---
+++ assets/AdminInboxPage-BrnkyE9M.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-BUuacDXO.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
