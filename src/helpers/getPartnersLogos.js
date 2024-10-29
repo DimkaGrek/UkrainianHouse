3a6f00cdfbd712c92426226d876b2f37cafe1dd0
VasylPopaz <pafos1997@gmail.com>
2024-10-29 17:58:01 +0100

    convert images to WebP for better performance

--- src/helpers/getPartnersLogos.js
+++ src/helpers/getPartnersLogos.js
@@ -1,56 +1,21 @@
-import logo1Img1 from '../assets/images/home/partners/Partner1@1x.png';
-import logo1Img2 from '../assets/images/home/partners/Partner1@2x.png';
-
-import logo2Img1 from '../assets/images/home/partners/Partner2@1x.png';
-import logo2Img2 from '../assets/images/home/partners/Partner2@2x.png';
-
-import logo3Img1 from '../assets/images/home/partners/Partner3@1x.png';
-import logo3Img2 from '../assets/images/home/partners/Partner3@2x.png';
-
-import logo4Img1 from '../assets/images/home/partners/Partner4@1x.png';
-import logo4Img2 from '../assets/images/home/partners/Partner4@2x.png';
-
-import logo5Img1 from '../assets/images/home/partners/Partner5@1x.png';
-import logo5Img2 from '../assets/images/home/partners/Partner5@2x.png';
-
-import logo6Img1 from '../assets/images/home/partners/Partner6@1x.png';
-import logo6Img2 from '../assets/images/home/partners/Partner6@2x.png';
-
-import logo7Img1 from '../assets/images/home/partners/Partner7@1x.png';
-import logo7Img2 from '../assets/images/home/partners/Partner7@2x.png';
-
-import logo8Img1 from '../assets/images/home/partners/Partner8@1x.png';
-import logo8Img2 from '../assets/images/home/partners/Partner8@2x.png';
-
-import logo9Img1 from '../assets/images/home/partners/Partner9@1x.png';
-import logo9Img2 from '../assets/images/home/partners/Partner9@2x.png';
-
-import logo10Img1 from '../assets/images/home/partners/Partner10@1x.png';
-import logo10Img2 from '../assets/images/home/partners/Partner10@2x.png';
-
-import logo11Img1 from '../assets/images/home/partners/Partner11@1x.png';
-import logo11Img2 from '../assets/images/home/partners/Partner11@2x.png';
-
-import logo12Img1 from '../assets/images/home/partners/Partner12@1x.png';
-import logo12Img2 from '../assets/images/home/partners/Partner12@2x.png';
-
-import logo13Img1 from '../assets/images/home/partners/Partner13@1x.png';
-import logo13Img2 from '../assets/images/home/partners/Partner13@2x.png';
+import { images } from '../assets';
 
 export const getPartnersLogos = () => {
+  const { homeImages } = images;
+
   return [
-    { logos: [logo1Img1, logo1Img2] },
-    { logos: [logo2Img1, logo2Img2] },
-    { logos: [logo3Img1, logo3Img2] },
-    { logos: [logo4Img1, logo4Img2] },
-    { logos: [logo5Img1, logo5Img2] },
-    { logos: [logo6Img1, logo6Img2] },
-    { logos: [logo7Img1, logo7Img2] },
-    { logos: [logo8Img1, logo8Img2] },
-    { logos: [logo9Img1, logo9Img2] },
-    { logos: [logo10Img1, logo10Img2] },
-    { logos: [logo11Img1, logo11Img2] },
-    { logos: [logo12Img1, logo12Img2] },
-    { logos: [logo13Img1, logo13Img2] },
+    { logos: [homeImages.logo1Img1, homeImages.logo1Img2] },
+    { logos: [homeImages.logo2Img1, homeImages.logo2Img2] },
+    { logos: [homeImages.logo3Img1, homeImages.logo3Img2] },
+    { logos: [homeImages.logo4Img1, homeImages.logo4Img2] },
+    { logos: [homeImages.logo5Img1, homeImages.logo5Img2] },
+    { logos: [homeImages.logo6Img1, homeImages.logo6Img2] },
+    { logos: [homeImages.logo7Img1, homeImages.logo7Img2] },
+    { logos: [homeImages.logo8Img1, homeImages.logo8Img2] },
+    { logos: [homeImages.logo9Img1, homeImages.logo9Img2] },
+    { logos: [homeImages.logo10Img1, homeImages.logo10Img2] },
+    { logos: [homeImages.logo11Img1, homeImages.logo11Img2] },
+    { logos: [homeImages.logo12Img1, homeImages.logo12Img2] },
+    { logos: [homeImages.logo13Img1, homeImages.logo13Img2] },
   ];
 };
