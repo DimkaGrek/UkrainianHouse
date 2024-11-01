8fc98c9f743c4840d5ec7ed133cae4538564f259
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-11-01 21:33:09 +0200

    delete backend

--- backend/ukrainianhouse/src/main/java/com/ukraininahouse/ukrainianhouse/repo/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ukraininahouse.ukrainianhouse.repo;
-
-import com.ukraininahouse.ukrainianhouse.model.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
