8489eef426657e85fe2991ade8fe34a3b463dbc6
skabanets <79872707+skabanets@users.noreply.github.com>
2024-11-04 22:41:39 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@37acaed278ccc031bc12677d4795bf2f1586fd32 🚀

--- assets/useNews-BasBGVdl.js
+++
@@ -1,1 +0,0 @@
-import{a as s,s as r,x as w,y as N,z as g,C as u,b as i,D as p}from"./index-BZP2J-Xr.js";const d=()=>{const e=s(r),t=s(w),o=s(N),a=s(g),c=s(u),n=s(i),l=s(p);return{news:e,announceNews:t,page:o,totalPages:a,totalNews:c,isLoading:n,error:l}};export{d as u};
