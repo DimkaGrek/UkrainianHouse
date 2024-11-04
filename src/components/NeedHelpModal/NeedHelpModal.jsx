29fc039638b7459f32f6f8e6ca24e2e15c2b63a4
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-11-04 19:39:32 +0200

    corrected styles

--- src/components/NeedHelpModal/NeedHelpModal.jsx
+++ src/components/NeedHelpModal/NeedHelpModal.jsx
@@ -5,8 +5,8 @@ export const NeedHelpModal = ({ toggleModal }) => {
     <div className="relative help-modal">
       <Icon
         name="heart"
-        size={60}
-        className="hidden lg:block absolute lg:-top-[130px] lg:-left-[65px] fill-[#f4faff] -rotate-45"
+        size={65}
+        className="absolute top-[5px] left-[8px] lg:-top-[130px] lg:-left-[65px] fill-[#f4faff] -rotate-45"
         viewbox="36"
       />
       <h3 className="pt-[340px] lg:pt-0 text-[20px] text-[#1e1e1e] leading-[1.2] lg:leading-[1.6] font-semibold mb-[8px] lg:mb-[18px]">
@@ -18,7 +18,7 @@ export const NeedHelpModal = ({ toggleModal }) => {
       </p>
       <button
         type="button"
-        className="w-full lg:w-[160px] rounded-[10px] h-[44px] lg:h-[52px] border border-[#1e1e1e] py-[10px] lg:py-[9px] text-[16px] lg:text-[20px] text-[#1e1e1e] text-center leading-[1] font-bold"
+        className="w-full lg:w-[160px] rounded-[10px] h-[44px] lg:h-[52px] border border-[#1e1e1e] py-[10px] lg:py-[9px] text-[16px] lg:text-[20px] text-[#1e1e1e] text-center leading-[1] font-bold hover:bg-[#ffd437] focus-visible:bg-[#ffd437] transition duration-300"
         onClick={toggleModal}
       >
         Sounds good
