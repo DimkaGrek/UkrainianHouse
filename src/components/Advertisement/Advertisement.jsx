197964a4acaf5b6972e0e982678eedac04df9405
VasylPopaz <pafos1997@gmail.com>
2024-11-04 12:48:33 +0100

    upd image styles for Advertisement

--- src/components/Advertisement/Advertisement.jsx
+++ src/components/Advertisement/Advertisement.jsx
@@ -9,7 +9,7 @@ export const Advertisement = () => {
   return announceNews?.length ? (
     <>
       <div className="flex flex-col-reverse gap-[16px] bg-[#fff5c6] p-[18px] rounded-[18px] mb-[34px] md:flex-row md:gap-[12px] md:p-[24px] md:bg-[#ffd437] md:bg-opacity-40 lg:gap-[40px] lg:bg-opacity-50 lg:py-[57px] lg:px-[40px] lg:mb-[44px] shadow-sm">
-        <div className="lg:w-[748px] flex flex-col  min-h-full">
+        <div className="lg:max-w-[748px] flex flex-col  min-h-full">
           <h3 className="font-proza-semibold text-[20px] font-semibold text-[#1e1e1e] mb-[4px] leading-[160%] md:font-proza-medium md:font-medium md:text-[26px] md:leading-[131%] md:mb-[8px] lg:font-proza-semibold lg:font-semibold lg:text-[36px] lg:leading-[111%] lg:mb-[20px] ">
             Coming soon...
           </h3>
@@ -23,7 +23,7 @@ export const Advertisement = () => {
         </div>
 
         <img
-          className="md:max-w-none md:w-[300px] lg:w-[600px] max-h-[400px] rounded-[10px] md:rounded-[18px]"
+          className="shrink-0 md:max-w-none md:w-[300px] lg:w-[450px] lg:max-w-[500px] max-h-[450px] rounded-[10px] md:rounded-[18px]"
           src={announceNews[0]?.photoUrls[0]?.photoUrls}
           alt="People"
           width="308"
