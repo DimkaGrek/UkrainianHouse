46762f548166e5b74d7d1bb37505cec27ad9d613
Sergey Kabanets <skabanets97@gmail.com>
2024-11-04 14:15:43 +0200

    Update styles for content list

--- src/components/ContentList/ContentList.jsx
+++ src/components/ContentList/ContentList.jsx
@@ -2,7 +2,10 @@ import { ContentListItem } from './ContentListItem';
 
 export const ContentList = ({ items }) => {
   return (
-    <ul className="pr-2 flex flex-col h-[70vh] max-h-[70vh] scrollbar mb-6">
+    <ul
+      className="pr-2 flex flex-col scrollbar mb-6"
+      style={{ height: 'calc(100vh - 258px)' }}
+    >
       {items.map(item => (
         <ContentListItem key={item.id} item={item} />
       ))}
