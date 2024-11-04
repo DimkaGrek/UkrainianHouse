581030f1cb88ec9e6bb0c8e91e923cc905c11c71
NiukaloTetiana <146371274+NiukaloTetiana@users.noreply.github.com>
2024-11-04 17:43:20 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@4e266697e4d3f27742689d6d67ab101538477885 🚀

--- assets/AdminInboxPage-BJe7DTnu.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CWCqB1-d.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
---
+++ assets/AdminInboxPage-BOmVRycp.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-BZP2J-Xr.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
