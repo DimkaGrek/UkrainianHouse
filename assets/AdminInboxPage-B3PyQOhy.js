19d2988b6a7990701a50a2b07b036de540daa673
skabanets <79872707+skabanets@users.noreply.github.com>
2024-11-04 12:26:51 +0000

    Deploying to gh-pages from @ DimkaGrek/UkrainianHouse@7de34511a09906d230661b71c4573facd8332332 🚀

--- assets/AdminInboxPage-B3PyQOhy.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CoNEQZgt.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
---
+++ assets/AdminInboxPage-BJe7DTnu.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CWCqB1-d.js";const o=()=>n.jsx("div",{children:"AdminInboxPage"});export{o as default};
