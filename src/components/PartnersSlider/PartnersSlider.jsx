399bb73fa7a9966635446c6b2c0f2169e3b16918
Anna Zubakha <korepovaanna@gmail.com>
2024-11-05 19:53:46 +0100

    change slider

--- src/components/PartnersSlider/PartnersSlider.jsx
+++ src/components/PartnersSlider/PartnersSlider.jsx
@@ -1,37 +1,24 @@
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Marquee from 'react-fast-marquee';
 import { getPartnersLogos } from '../../helpers';
-import Slider from 'react-slick';
+
 export const PartnersSlider = () => {
   const partnersLogos = getPartnersLogos();
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    arrows: false,
-    autoplaySpeed: 0,
-    cssEase: 'linear',
-    draggable: false,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-  };
   return (
     <div className="slider-custom">
-      <Slider {...settings}>
-        {partnersLogos.map(({ logos }, index) => (
-          <div key={index} className="slider-item-custom">
-            <img
-              src={logos[0]}
-              srcSet={`${logos[0]} 1x, ${logos[1]} 2x`}
-              alt="Partner's logo"
-            />
-          </div>
-        ))}
-      </Slider>
+      <Marquee direction="left">
+        <ul className="flex justify-center items-center gap-2 md:gap-[48px] mr-2 md:mr-[48px]">
+          {partnersLogos.map(({ logos }, index) => (
+            <li key={index} className="slider-item-custom">
+              <img
+                src={logos[0]}
+                srcSet={`${logos[0]} 1x, ${logos[1]} 2x`}
+                alt="Partner's logo"
+              />
+            </li>
+          ))}
+        </ul>
+      </Marquee>
     </div>
   );
 };
