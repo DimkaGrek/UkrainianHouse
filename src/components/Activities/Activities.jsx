76ba7d8c2ee7e49768dfe0785aeac3f54599a0b1
Sergey Kabanets <skabanets97@gmail.com>
2024-11-17 10:16:15 +0200

    Add useResizeAndState hook to optimize AboutUs and Activities components

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,63 +1,42 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 
-import { Icon } from "components";
+import { Icon } from "../../components";
+
+import { useResizeAndState } from "../../hooks";
+import { getIconSize } from "../../helpers";
 
 export const Activities = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const getIconSize = () => {
-    if (windowWidth >= 1440) {
-      return 40;
-    } else {
-      return 15;
-    }
-  };
+  const { windowWidth } = useResizeAndState();
+  
+  const isonSize = getIconSize(windowWidth);
 
   return (
-    <ul className="font-proza-medium md:grid-cols-1 md:grid-rows-4 mb-[44px] flex flex-col gap-[16px] text-[20px] font-[500] leading-[1.2] md:grid md:gap-[24px] lg:absolute lg:left-[586px] lg:top-[-180px] lg:mb-0 lg:gap-[34px] lg:gap-x-[84px] lg:gap-y-[24px] lg:leading-[1.6]">
-      <li className="flex items-center gap-[8px] font-[600] md:col-start-1 lg:gap-[24px]">
-        <Icon size={getIconSize()} />
+    <ul className="font-proza-medium md:grid-cols-1 md:grid-rows-4 mb-[44px] flex flex-col gap-[16px] text-[20px] font-semibold leading-[1.2] md:grid md:gap-[24px] lg:absolute lg:left-[586px] lg:top-[-180px] lg:mb-0 lg:gap-[34px] lg:gap-x-[84px] lg:gap-y-[24px] lg:leading-[1.6]">
+      <li className="flex items-center gap-[8px] md:col-start-1 lg:gap-[24px]">
+        <Icon size={isonSize} />
         Ukrainian Echo
       </li>
-      <li className="flex items-center gap-[8px] md:col-start-1 lg:gap-[24px]">
-        {" "}
-        <Icon size={getIconSize()} /> Dutch Lessons
+      <li className="flex items-center gap-[8px] font-[600] md:col-start-1 lg:gap-[24px]">
+        <Icon size={isonSize} /> Dutch Lessons
       </li>
       <li className="flex items-center gap-[8px] md:col-start-1 lg:gap-[24px]">
-        <Icon size={getIconSize()} />
+        <Icon size={isonSize} />
         Cultural Events
       </li>
       <li className="flex items-center gap-[8px] md:col-start-1 lg:gap-[24px]">
-        {" "}
-        <Icon size={getIconSize()} /> Integration Lectures
+        <Icon size={isonSize} /> Integration Lectures
       </li>
       <li className="flex items-center gap-[8px] md:col-start-2 md:row-start-1 lg:gap-[24px]">
-        {" "}
-        <Icon size={getIconSize()} /> Creative Workshops
+        <Icon size={isonSize} /> Creative Workshops
       </li>
       <li className="flex items-center gap-[8px] md:col-start-2 md:row-start-2 lg:gap-[24px]">
-        {" "}
-        <Icon size={getIconSize()} /> Yoga Sessions
+        <Icon size={isonSize} /> Yoga Sessions
       </li>
-      <li className="flex items-center gap-[8px] md:col-start-2 md:row-start-3 lg:gap-[24px]">
-        {" "}
-        <Icon size={getIconSize()} /> Cinema
+      <li className="flex items-center gap-[8px] font-[600] md:col-start-2 md:row-start-3 lg:gap-[24px]">
+        <Icon size={isonSize} /> Cinema
       </li>
-      <li className="flex items-center gap-[8px] font-[600] md:col-start-2 md:row-start-4 lg:gap-[24px]">
-        <Icon size={getIconSize()} />
+      <li className="flex items-center gap-[8px] md:col-start-2 md:row-start-4 lg:gap-[24px]">
+        <Icon size={isonSize} />
         <Link to="/library" className="border-b-2 border-[#2355cc] text-[#2355cc]">
           Library
         </Link>
