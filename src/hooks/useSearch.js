2aa5b072215db0510ab17db03b2fe3e96f0433f9
VasylPopaz <pafos1997@gmail.com>
2024-11-17 17:53:31 +0100

    upd useSearch hook

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useLocation, useSearchParams } from "react-router-dom";
 
+import { useWindowSizeHook } from "../hooks";
+
 export const useSearch = (setQuery) => {
+  const [placeholder, setPlaceholder] = useState("Search...");
+
   const { watch, register, setValue, reset, handleSubmit } = useForm();
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
+  const { innerWidth } = useWindowSizeHook();
 
   useEffect(() => {
     reset();
@@ -17,6 +22,16 @@ export const useSearch = (setQuery) => {
       setValue("query", keyword);
     }
   }, [searchParams, setValue]);
+
+  useEffect(() => {
+    if (location.pathname === "/library" && innerWidth < 1440) {
+      setPlaceholder("Search book...");
+    } else if (location.pathname === "/library" && innerWidth >= 1440) {
+      setPlaceholder("Search by title, author, or genre...");
+    } else {
+      setPlaceholder("Search...");
+    }
+  }, [innerWidth, location.pathname]);
 
   const onSubmit = ({ query }) => {
     if (!query) return;
@@ -33,6 +48,7 @@ export const useSearch = (setQuery) => {
 
   return {
     query,
+    placeholder,
     register,
     handleSubmit,
     onSubmit,
