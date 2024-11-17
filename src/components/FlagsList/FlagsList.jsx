e55dc2a57da7a8e93f1ba011c9467d0619cfc2e2
Sergey Kabanets <skabanets97@gmail.com>
2024-11-17 10:52:47 +0200

    Refactor FlagsList component

--- src/components/FlagsList/FlagsList.jsx
+++ src/components/FlagsList/FlagsList.jsx
@@ -2,39 +2,27 @@ import { images } from "../../assets";
 
 export const FlagsList = ({ className }) => {
   const { headerImages } = images;
+
+   const flagData = [
+    { key: 'eng', alt: 'Flag of England' },
+    { key: 'nl', alt: 'Flag of Netherlands' },
+    { key: 'ua', alt: 'Flag of Ukraine' }
+  ];
   
   return (
     <ul className={className}>
-      <li>
-        <img
-          srcSet={`${headerImages.flag_eng1x} 1x, ${headerImages.flag_eng2x} 2x`}
-          src={headerImages.flag_eng1x}
-          alt="Flag of England"
-          width="40"
-          height="40"
-          loading="lazy"
-        />
-      </li>
-      <li>
-        <img
-          srcSet={`${headerImages.flag_nl1x} 1x, ${headerImages.flag_nl2x} 2x`}
-          src={headerImages.flag_nl1x}
-          alt="Flag of Netherland"
-          width="40"
-          height="40"
-          loading="lazy"
-        />
-      </li>
-      <li>
-        <img
-          srcSet={`${headerImages.flag_ua1x} 1x, ${headerImages.flag_ua2x} 2x`}
-          src={headerImages.flag_ua1x}
-          alt="Flag of Ukraine"
-          width="40"
-          height="40"
-          loading="lazy"
-        />
-      </li>
+      {flagData.map(({ key, alt }) => (
+        <li key={key}>
+          <img
+            srcSet={`${headerImages[`flag_${key}1x`]} 1x, ${headerImages[`flag_${key}2x`]} 2x`}
+            src={headerImages[`flag_${key}1x`]}
+            alt={alt}
+            width="40"
+            height="40"
+            loading="lazy"
+          />
+        </li>
+      ))}
     </ul>
   );
 };
